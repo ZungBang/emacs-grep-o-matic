3725e3cdc346d9ea26b50f1b067e4ddbf076bb3e
Avi Rozen <avi.rozen@gmail.com>
2021-06-18 17:14:17 +0300

    updated copyright years and minor version

--- grep-o-matic.el
+++ grep-o-matic.el
@@ -1,11 +1,11 @@
 ;;; grep-o-matic.el --- auto grep word under cursor
 
-;; Copyright (C) 2008-2013 Avi Rozen
+;; Copyright (C) 2008-2021 Avi Rozen
 
 ;; Author: Avi Rozen <avi.rozen@gmail.com>
 ;; Keywords: tools, processes, search
 ;; URL: https://github.com/ZungBang/emacs-grep-o-matic
-;; Version: 1.0.6
+;; Version: 1.0.7
 
 ;; This file is NOT part of GNU Emacs.
 
